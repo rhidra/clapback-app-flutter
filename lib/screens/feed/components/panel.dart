a7a0d22f854aeb1e895a4632235f8fea54819fac
Rémy Hidra <remy.hidra@gmail.com>
2020-12-25 19:48:28 +0100

    Refactor topics panels

---
+++ lib/screens/feed/components/panel.dart
@@ -0,0 +1,72 @@
+import 'package:clapback_app/components/video-player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:clapback_app/components/error.dart';
+import 'package:clapback_app/models/panel.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:clapback_app/services/api-client.dart';
+
+class PanelWidget extends StatelessWidget {
+  final Panel _panel;
+
+  PanelWidget(this._panel);
+
+  @override
+  Widget build(BuildContext context) {
+    final int type = _panel.type;
+
+    if (type == Panel.VIDEO) {
+      return _buildVideoPanel(context);
+    } else if (type == Panel.TEXT) {
+      return _buildTextPanel(context);
+    } else if (type == Panel.QUIZ) {
+      return _buildQuizPanel(context);
+    } else {
+      return ErrorScreen(error: 'Unknown panel type !');
+    }
+  }
+
+  Widget _buildVideoPanel(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: VideoPlayerScreen(_panel.video),
+    );
+  }
+
+  Widget _buildTextPanel(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: Column(
+        children: [
+          // Image
+          Container(
+            constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height / 3),
+            child: CachedNetworkImage(
+              imageUrl: ApiClient.formatMedia(_panel.image, 'image'),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ),
+          // Text
+          Flexible(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  _panel.text,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 17),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQuizPanel(BuildContext context) {
+    return Center(child: CircularProgressIndicator());
+  }
+}
--- lib/screens/feed/components/topic.dart
+++ lib/screens/feed/components/topic.dart
@@ -1,11 +1,7 @@
-import 'package:clapback_app/components/error.dart';
-import 'package:clapback_app/components/video-player.dart';
-import 'package:clapback_app/models/panel.dart';
 import 'package:clapback_app/models/topic.dart';
-import 'package:clapback_app/services/api-client.dart';
+import 'package:clapback_app/screens/feed/components/panel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class TopicWidget extends StatefulWidget {
   final Topic topic;
@@ -39,40 +35,34 @@ class _TopicWidgetState extends State<TopicWidget> {
           controller: _pageCtrl,
           scrollDirection: Axis.horizontal,
           children: [
-            _buildPanel(widget.topic.leftPanel),
-            _buildPanel(widget.topic.centerPanel),
-            _buildPanel(widget.topic.rightPanel),
+            PanelWidget(widget.topic.leftPanel),
+            PanelWidget(widget.topic.centerPanel),
+            PanelWidget(widget.topic.rightPanel),
           ],
         ),
         Material(
           type: MaterialType.transparency,
           child: Align(
-            alignment: Alignment(1, .5),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  child: _buildSideButton(
+            alignment: Alignment(1, .1),
+            child: Padding(
+              padding: EdgeInsets.only(right: 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _buildSideButton(
                     icon: Icons.share,
                     onPressed: () {},
                   ),
-                  padding: EdgeInsets.all(10),
-                ),
-                Padding(
-                  child: _buildSideButton(
+                  _buildSideButton(
                     icon: Icons.favorite,
                     onPressed: () {},
                   ),
-                  padding: EdgeInsets.all(10),
-                ),
-                Padding(
-                  child: _buildSideButton(
+                  _buildSideButton(
                     icon: Icons.fast_rewind,
                     onPressed: () {},
                   ),
-                  padding: EdgeInsets.all(10),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -80,75 +70,24 @@ class _TopicWidgetState extends State<TopicWidget> {
     );
   }
 
-  Widget _buildPanel(Panel panel) {
-    final int type = panel.type;
-
-    if (type == Panel.VIDEO) {
-      return _buildVideoPanel(panel);
-    } else if (type == Panel.TEXT) {
-      return _buildTextPanel(panel);
-    } else if (type == Panel.QUIZ) {
-      return _buildQuizPanel(panel);
-    } else {
-      return ErrorScreen(error: 'Unknown panel type !');
-    }
-  }
-
-  Widget _buildVideoPanel(Panel panel) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: VideoPlayerScreen(panel.video),
-    );
-  }
-
-  Widget _buildTextPanel(Panel panel) {
-    return ColoredBox(
-      color: Colors.white,
-      child: Column(
-        children: [
-          // Image
-          Container(
-            constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height / 3),
-            child: CachedNetworkImage(
-              imageUrl: ApiClient.formatMedia(panel.image, 'image'),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ),
-          // Text
-          Flexible(
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  panel.text,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 17),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildQuizPanel(Panel panel) {
-    return Center(child: CircularProgressIndicator());
-  }
-
   Widget _buildSideButton({IconData icon, Function onPressed}) {
-    return Ink(
-      child: IconButton(
-        icon: Icon(icon),
-        color: Color(0xFF222222),
-        onPressed: onPressed,
-      ),
-      decoration: ShapeDecoration(
-        color: Color(0x88FFFFFF),
-        shape: CircleBorder(),
-        shadows: [BoxShadow(blurRadius: 2, color: Color(0x77000000))],
+    return Padding(
+      padding: EdgeInsets.only(top: 8),
+      child: Ink(
+        child: SizedBox(
+          height: 40,
+          child: IconButton(
+            icon: Icon(icon),
+            color: Color(0xFF222222),
+            onPressed: onPressed,
+            iconSize: 20,
+          ),
+        ),
+        decoration: ShapeDecoration(
+          color: Color(0x88FFFFFF),
+          shape: CircleBorder(),
+          shadows: [BoxShadow(blurRadius: 2, color: Color(0x77000000))],
+        ),
       ),
     );
   }
