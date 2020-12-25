df7494e1847ab27cea2fba5c4a426dfa92733a80
Rémy Hidra <remy.hidra@gmail.com>
2020-12-25 19:10:28 +0100

    Fix network for andorid emulator

--- lib/env.dart
+++ lib/env.dart
@@ -3,5 +3,6 @@ library env;
 import 'package:flutter/foundation.dart';
 
 const String protocol = kReleaseMode ? 'https' : 'http';
-const String host = kReleaseMode ? 'api.clapbacktheapp.com' : '192.168.43.29';
+const String host =
+    kReleaseMode ? 'api.clapbacktheapp.com' : /*'192.168.43.243'*/ '10.0.2.2';
 const int port = kReleaseMode ? 443 : 9000;
