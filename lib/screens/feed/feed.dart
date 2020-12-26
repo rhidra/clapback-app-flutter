879f242c24e94a457e116b39f8a5897ff325256e
Rémy Hidra <remy.hidra@gmail.com>
2020-12-26 17:53:49 +0100

    Add an error screen

--- lib/screens/feed/feed.dart
+++ lib/screens/feed/feed.dart
@@ -2,6 +2,7 @@ import 'package:clapback_app/bloc/news/news-bloc.dart';
 import 'package:clapback_app/bloc/news/news-event.dart';
 import 'package:clapback_app/bloc/news/news-state.dart';
 import 'package:clapback_app/components/drawer.dart';
+import 'package:clapback_app/components/error.dart';
 import 'package:clapback_app/screens/feed/components/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -43,10 +44,11 @@ class _FeedState extends State<Feed> {
           if (state is NewsStateLoading) {
             main = Center(child: CircularProgressIndicator());
           } else if (state is NewsStateNotConnected) {
-            main = Center(
-                child: CircularProgressIndicator(
-              backgroundColor: Colors.green,
-            ));
+            main = ErrorScreen(
+              error:
+                  'The server is unreachable. Are you connected to the internet ?',
+              icon: Icons.wifi_off,
+            );
           } else if (state is NewsStateError) {
             main = Text(state.error);
           } else if (state is NewsStateSuccess) {
