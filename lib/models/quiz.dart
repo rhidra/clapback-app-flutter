b29f8d1bc43397769367d29dff2966a97aa8ee70
Rémy Hidra <remy.hidra@gmail.com>
2020-12-28 17:51:30 +0100

    Add quiz choices model

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -13,14 +13,34 @@ class Quiz {
   final bool isPoll;
   final String explanationText;
 
+  final List<QuizChoice> choices;
+
   Quiz(
       {this.question,
       this.content,
       this.topic,
       this.isPoll,
       this.explanationText,
+      this.choices,
       this.id});
 
   factory Quiz.fromJson(Map<String, dynamic> json) => _$QuizFromJson(json);
   Map<String, dynamic> toJson() => _$QuizToJson(this);
+}
+
+@JsonSerializable()
+class QuizChoice {
+  @JsonKey(name: '_id')
+  final String id;
+
+  final String text;
+  final String color;
+  final bool goodAnswer;
+  final int count;
+
+  QuizChoice({this.id, this.text, this.color, this.goodAnswer, this.count});
+
+  factory QuizChoice.fromJson(Map<String, dynamic> json) =>
+      _$QuizChoiceFromJson(json);
+  Map<String, dynamic> toJson() => _$QuizChoiceToJson(this);
 }
--- lib/models/quiz.g.dart
+++ lib/models/quiz.g.dart
@@ -13,6 +13,10 @@ Quiz _$QuizFromJson(Map<String, dynamic> json) {
     topic: json['topic'] as String,
     isPoll: json['isPoll'] as bool,
     explanationText: json['explanationText'] as String,
+    choices: (json['choices'] as List)
+        ?.map((e) =>
+            e == null ? null : QuizChoice.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
     id: json['_id'] as String,
   );
 }
@@ -24,4 +28,24 @@ Map<String, dynamic> _$QuizToJson(Quiz instance) => <String, dynamic>{
       'topic': instance.topic,
       'isPoll': instance.isPoll,
       'explanationText': instance.explanationText,
+      'choices': instance.choices,
+    };
+
+QuizChoice _$QuizChoiceFromJson(Map<String, dynamic> json) {
+  return QuizChoice(
+    id: json['_id'] as String,
+    text: json['text'] as String,
+    color: json['color'] as String,
+    goodAnswer: json['goodAnswer'] as bool,
+    count: json['count'] as int,
+  );
+}
+
+Map<String, dynamic> _$QuizChoiceToJson(QuizChoice instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'text': instance.text,
+      'color': instance.color,
+      'goodAnswer': instance.goodAnswer,
+      'count': instance.count,
     };
